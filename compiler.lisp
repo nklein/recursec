d1c5593ad49e6722aa77d8db6503223516385df9
Patrick Stein <pat@nklein.com>
2015-03-06 09:08:23 -0600

    CL-USER> (asdf:test-system :recursec) Running package "RECURSEC-TESTS" Running group RECURSEC-TESTS::RECURSEC-EXPORTS Running group RECURSEC-TESTS::RECURSEC-DOCUMENTATION Package RECURSEC-TESTS: 3 of 4 passed  - Group RECURSEC-EXPORTS: 2 of 3 passed     - Check LOAD-REGISTER failed        - Value 0 not equal to value of 1  - Group RECURSEC-DOCUMENTATION: 1 of 1 passed TOTAL: 3 of 4 passed (1 failed, 0 errors, 0 warnings)

--- compiler.lisp
+++ compiler.lisp
@@ -1,8 +1,11 @@
 (in-package #:recursec)
 
+(defvar *register* 0
+  "The register in the Recurse virtual machine.")
+
 (defun compile-function (&rest lines)
   (let ((name (intern (subseq (first lines) 0 1))))
     (setf (symbol-function name)
           (lambda ()
-            1))
+            *register*))
     name))
