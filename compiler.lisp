19595bea07b4111388458c1058044b11bc3855a2
Patrick Stein <pat@nklein.com>
2015-03-06 13:57:12 -0600

    CL-USER> (asdf:test-system :recursec) Running package "RECURSEC-TESTS" Running group RECURSEC-TESTS::RECURSEC-EXPORTS Running group RECURSEC-TESTS::RECURSEC-DOCUMENTATION Running group RECURSEC-TESTS::RECURSEC-COMPILER Package RECURSEC-TESTS: 7 of 7 passed  - Group RECURSEC-EXPORTS: 1 of 1 passed  - Group RECURSEC-DOCUMENTATION: 1 of 1 passed  - Group RECURSEC-COMPILER: 5 of 5 passed TOTAL: 7 of 7 passed (0 failed, 0 errors, 0 warnings)

--- compiler.lisp
+++ compiler.lisp
@@ -3,6 +3,24 @@
 (defvar *register* 0
   "The register in the Recurse virtual machine.")
 
+(defun make-position (x y)
+  (list x y))
+
+(defun x (position)
+  (first position))
+
+(defun y (position)
+  (second position))
+
+(defun make-direction (dx dy)
+  (list dx dy))
+
+(defun dx (direction)
+  (first direction))
+
+(defun dy (direction)
+  (second direction))
+
 (defun find-easterly-entry-point (lines)
   (loop :for line :in lines
      :for line-number :from 0
@@ -13,13 +31,35 @@
   (anaphora:awhen (digit-char-p ch)
     `(setf *register* ,anaphora:it)))
 
-(defun extract-forms-starting-at (lines line-number position-number)
-  (loop :with line = (elt lines line-number)
-     :for position :from position-number :below (length line)
-     :for ch = (elt line position)
-     :for form = (get-form-for-char ch)
-     :when form
-     :collect form))
+(defun get-form-direction (ch)
+  (case ch
+    (#\>       (make-direction 1 0))
+    (#\<       (make-direction -1 0))
+    ((#\v #\V) (make-direction 0 1))
+    (#\^       (make-direction 0 -1))))
+
+(defun extract-forms-starting-at (lines row column)
+  (labels ((in-bounds (position bounds)
+             (and (<= 0 (x position) (x bounds))
+                  (<= 0 (y position) (y bounds))))
+
+           (increment-position (position direction)
+             (make-position (+ (x position) (dx direction))
+                            (+ (y position) (dy direction)))))
+
+    (loop :with bounds = (make-position (1- (length (first lines)))
+                                        (1- (length lines)))
+       :for direction = (make-direction 1 0)
+         :then (or (get-form-direction ch)
+                   direction)
+       :for position = (make-position column row)
+         :then (increment-position position direction)
+       :while (in-bounds position bounds)
+       :for line = (elt lines (y position))
+       :for ch = (elt line (x position))
+       :for form = (get-form-for-char ch)
+       :when form
+       :collect form)))
 
 (defun function-forms (lines)
   (anaphora:awhen (find-easterly-entry-point lines)
