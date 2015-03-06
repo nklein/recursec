f03b75900d6c4d7e92a07d94813c0ba41bf4de04
Patrick Stein <pat@nklein.com>
2015-03-05 22:27:03 -0600

    CL-USER> (asdf:test-system :recursec) Running package "RECURSEC-TESTS" Running group RECURSEC-TESTS::RECURSEC-EXPORTS Running group RECURSEC-TESTS::RECURSEC-DOCUMENTATION Package RECURSEC-TESTS: 1 of 2 passed  - Group RECURSEC-EXPORTS: 0 of 1 passed     - Check COMPILE-SIMPLEST-FUNCTION failed        - Value ("B" ">" "B") not equal to value of 'RECURSEC-TESTS::B  - Group RECURSEC-DOCUMENTATION: 1 of 1 passed TOTAL: 1 of 2 passed (1 failed, 0 errors, 0 warnings)

--- tests/compiler.lisp
+++ tests/compiler.lisp
@@ -1,7 +1,8 @@
 (in-package #:recursec-tests)
 
 (nst:def-test-group recursec-exports ()
-  (nst:def-test compile-simplest-function (:true)
-    (recursec:compile-function "B"
-                               ">"
-                               "B")))
+  (nst:def-test compile-simplest-function (:equal 'b)
+    (let ((*package* (find-package '#:recursec-tests)))
+      (recursec:compile-function "B"
+                                 ">"
+                                 "B"))))
