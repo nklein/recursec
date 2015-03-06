be36d190b4708ebcacb55f6a005f43504fa90aa3
Patrick Stein <pat@nklein.com>
2015-03-05 22:38:54 -0600

    CL-USER> (asdf:test-system :recursec) Running package "RECURSEC-TESTS" Running group RECURSEC-TESTS::RECURSEC-EXPORTS Running group RECURSEC-TESTS::RECURSEC-DOCUMENTATION Package RECURSEC-TESTS: 1 of 2 passed  - Group RECURSEC-EXPORTS: 0 of 1 passed     - Check COMPILE-SIMPLEST-FUNCTION failed        - Predicate FBOUNDP fails for (RECURSEC-TESTS::B)  - Group RECURSEC-DOCUMENTATION: 1 of 1 passed TOTAL: 1 of 2 passed (1 failed, 0 errors, 0 warnings)

--- tests/compiler.lisp
+++ tests/compiler.lisp
@@ -1,7 +1,8 @@
 (in-package #:recursec-tests)
 
 (nst:def-test-group recursec-exports ()
-  (nst:def-test compile-simplest-function (:equal 'b)
+  (nst:def-test compile-simplest-function (:all (:equal 'b)
+                                                (:predicate fboundp))
     (let ((*package* (find-package '#:recursec-tests)))
       (recursec:compile-function "B"
                                  ">"
