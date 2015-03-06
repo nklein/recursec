6f6c399cf6a04a67c20184cfeb54e814964203ad
Patrick Stein <pat@nklein.com>
2015-03-06 09:19:49 -0600

    CL-USER> (asdf:test-system :recursec) Running package "RECURSEC-TESTS" Running group RECURSEC-TESTS::RECURSEC-EXPORTS Running group RECURSEC-TESTS::RECURSEC-DOCUMENTATION Package RECURSEC-TESTS: 4 of 4 passed  - Group RECURSEC-EXPORTS: 3 of 3 passed  - Group RECURSEC-DOCUMENTATION: 1 of 1 passed TOTAL: 4 of 4 passed (0 failed, 0 errors, 0 warnings)

--- compiler.lisp
+++ compiler.lisp
@@ -3,9 +3,17 @@
 (defvar *register* 0
   "The register in the Recurse virtual machine.")
 
+(defun function-forms (lines)
+  (loop :for line :in lines
+     :appending (loop :for ch :in (coerce line 'list)
+                   :for digit = (digit-char-p ch)
+                   :when digit
+                   :collect `(setf *register* ,digit))))
+
 (defun compile-function (&rest lines)
   (let ((name (intern (subseq (first lines) 0 1))))
     (setf (symbol-function name)
-          (lambda ()
-            *register*))
+          (compile nil `(lambda ()
+                          ,@(function-forms lines)
+                          *register*)))
     name))
